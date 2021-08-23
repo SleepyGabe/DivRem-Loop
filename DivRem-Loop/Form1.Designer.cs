0d744842bef6d47a38dd45a77313c4f8e561a119
Gabe <89386562+SleepyGabe@users.noreply.github.com>
2021-08-23 18:06:07 +0800

    Add files via upload
    This time, the spaghetti code is actually there

---
+++ DivRem-Loop/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace OTFI1B_Parinas_DivRemLoop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Num1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Num2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtQuot = new System.Windows.Forms.TextBox();
+            this.TxtRem = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Math suxx";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Dividend:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(25, 60);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(104, 22);
+            this.Num1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(199, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Divisor:";
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(202, 60);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(104, 22);
+            this.Num2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(89, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Divide the thing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quotient:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(199, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Remainder:";
+            // 
+            // TxtQuot
+            // 
+            this.TxtQuot.Location = new System.Drawing.Point(29, 172);
+            this.TxtQuot.Name = "TxtQuot";
+            this.TxtQuot.ReadOnly = true;
+            this.TxtQuot.Size = new System.Drawing.Size(100, 22);
+            this.TxtQuot.TabIndex = 8;
+            // 
+            // TxtRem
+            // 
+            this.TxtRem.Location = new System.Drawing.Point(202, 172);
+            this.TxtRem.Name = "TxtRem";
+            this.TxtRem.ReadOnly = true;
+            this.TxtRem.Size = new System.Drawing.Size(100, 22);
+            this.TxtRem.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(156, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(355, 260);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtRem);
+            this.Controls.Add(this.TxtQuot);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Num1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Num2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtQuot;
+        private System.Windows.Forms.TextBox TxtRem;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DivRem-Loop/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTFI1B_Parinas_DivRemLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Goal: Get the quotient and remainder from a given divisor and dividend minus the use of modulo and 
+            //stuff like that-
+            //1.) Declare variables for Divisor, Dividend, Quotient, and Remainder
+            int Div1, Div2, RemNum, QuoNum = 0;
+
+            //2A.) Check for any blank/invalid entries
+            //2B.) Declare variables for both input boxes
+            bool Boxxy, BoxxyJr;
+
+            Boxxy = int.TryParse(Num1.Text, out Div1);
+
+            if (Boxxy == false)
+            {
+                MessageBox.Show("Please enter a VALID NUMBER for the Dividend");
+                Num1.Clear();
+                Num1.Select();
+                return;
+            }
+            BoxxyJr = int.TryParse(Num2.Text, out Div2);
+
+            if (BoxxyJr == false)
+            {
+                MessageBox.Show("Please enter a VALID NUMBER for the Divisor");
+                Num2.Clear();
+                Num2.Select();
+                return;
+            }
+            //3.) Retrieve inputs from user
+            Div1 = int.Parse(Num1.Text);
+            Div2 = int.Parse(Num2.Text);
+            //4.) Divide 
+            if (Div1 > Div2)
+            {
+                while (Div1 >= Div2)
+                {
+                    Div1 = Div1 - Div2;
+                    QuoNum++;
+                }
+                RemNum = Div1;
+                TxtQuot.Text = QuoNum.ToString();
+                TxtRem.Text = RemNum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Enter a VALID input");
+            }
+        }
+    }
+}
